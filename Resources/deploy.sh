cf817c58b2afbee3d69890ed01c0b5295aff390d
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-07-07 10:34:23 +0530

    deploy

--- Resources/deploy.sh
+++ Resources/deploy.sh
@@ -1,10 +1,4 @@
-
-# Ultroid - UserBot
-# Copyright (C) 2020 ULTROID-OP
-#
-# This file is a part of < https://github.com/ULTROID-OP/ULTROID-BOT/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/ULTROID-OP/Ultroid/blob/main/LICENSE/>.
+# Copyright Team ExtremePro 2021-2022
 
 echo "
 	      ╔╦╦╦══╦═╦═╦══╦══╗
