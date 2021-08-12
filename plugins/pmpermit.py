ecb111878b57e4f72700f659ad511253930059b8
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-12 18:53:14 +0530

    K

--- plugins/pmpermit.py
+++ plugins/pmpermit.py
@@ -175,8 +175,9 @@ async def on_new_private_message(event):
         # userbot's should not reply to other userbot's
         # https://core.telegram.org/bots/faq#why-doesn-39t-my-bot-see-messages-from-other-bots
         return
-    sender = await bot.get_entity(chat_ids)
     chat_ids = event.chat_id
+    sender = await bot.get_entity(chat_ids)
+    
 
     if chat_ids == Andencento.uid:
 
