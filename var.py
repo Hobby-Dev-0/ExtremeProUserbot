4e01df5d5004fb6fe8c513b4a7cd1aeefa2c4c1d
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-13 17:38:18 +0530

    iampro

---
+++ var.py
@@ -0,0 +1,11 @@
+import os
+
+ENV = bool(os.environ.get("ENV", False))
+if ENV:
+    from Extre.config import Var as Config
+    from Extre.var import Var
+else:
+    from local_config import Development as Config
+
+
+Var = Config
